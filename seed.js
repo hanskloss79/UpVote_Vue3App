b83848438ffb45f0d6c30bd8fc0b8a9d588d913a
hanskloss79 <piohandz79@gmail.com>
2023-03-09 12:42:33 +0100

    The UpVote Vue 3 App created in main.js. List of submissions created in index.html by using v-for and v-bind directives.

---
+++ seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+  const submissions = [
+    {
+      id: 1,
+      title: 'Żółte wiadro',
+      description: 'Ekspertyza w zakresie budowy zamków z piasku na żądanie :)',
+      url: '#',
+      votes: 16,
+      avatar: './public/images/avatars/daniel.jpg',
+      submissionImage: './public/images/submissions/image-yellow.png',
+    },
+    {
+      id: 2,
+      title: 'Supermajority: Liga kongresu fantastyki',
+      description: 'Zdobywaj punkty, gdy Twoi ulubieni politycy uchwalają przepisy.',
+      url: '#',
+      votes: 11,
+      avatar: './public/images/avatars/kristy.png',
+      submissionImage: './public/images/submissions/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Dopasowane czapki z folii aluminiowej',
+      description: 'Mamy Twoje wymiary i adres do wysyłki.',
+      url: '#',
+      votes: 17,
+      avatar: './public/images/avatars/veronika.jpg',
+      submissionImage: './public/images/submissions/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'Zarozumiały czy roztargniony? Ty decydujesz.',
+      url: '#',
+      votes: 9,
+      avatar: './public/images/avatars/molly.png',
+      submissionImage: './public/images/submissions/image-aqua.png',
+    }
+  ];
+
+  return { submissions: submissions };
+}());
